dfb8bf1d50aba8223a29dd98bad6117e673d0fa3
Snow <snow@hollysydney.com>
2019-03-06 15:55:34 +1100

    Tests for the update mechanism.

--- js/wcall.js
+++ js/wcall.js
@@ -28,10 +28,40 @@ var wcallcls = function(options) {
   }
 
   /*
+   * Update the data and then update the contents.
+   */
+  this.updateView = function() {
+    var self = this;
+
+    var container = jQuery("#" + opts.widgetid);
+    self.getData(function(data) {
+      jQuery("textarea", container).val(JSON.stringify(data, null, 2));
+    });
+  }
+
+  /*
+   * TESTING
+   * Create an update button.
+   */
+  this.test = function() {
+    var self = this;
+
+    var container = jQuery("#" + opts.widgetid);
+    jQuery(container).append("<button>Update</button>");
+    jQuery("button", container).on("click", function(e) {
+      jQuery("textarea", container).val("");
+      self.updateView();
+    });
+  }
+
+  /*
    * Constructor
    */
   this.construct = function(options){
     jQuery.extend(opts, options);
+
+    // Do any preflight items while in testing.
+    this.test();
   };
 
   /*
